259b9a148b1979daa04cda1a0fab1171831e57e5
Shiwarne Silva <103165392+Shiwarne-Silva@users.noreply.github.com>
2024-09-02 21:25:11 +0530

    Initial Commit

---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Shiwarne Silva on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
